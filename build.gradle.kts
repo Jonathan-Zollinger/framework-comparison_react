2891ffda1285b77fe743d02f7069fbbb811022a5
jonathan zollinger <jonathan.zollinger@outlook.com>
2025-02-08 23:31:42 -0700

    refactor: convert jvm lang to kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.25"
+    id("com.google.devtools.ksp") version "1.9.25-1.0.20"
     id("groovy") 
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.micronaut.application") version "4.4.4"
@@ -8,7 +11,7 @@ plugins {
 
 version = "0.0.1"
 group = "jonathan_zollinger"
-
+val kotlinVersion= project.properties["kotlinVersion"]
 repositories {
     mavenCentral()
 }
@@ -25,15 +28,14 @@ dependencies {
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("org.yaml:snakeyaml")
     testImplementation("io.micronaut:micronaut-http-client")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 
 application {
-    mainClass = "jonathan_zollinger.Application"
+    mainClass = "jonathan_zollinger.Application.kt"
 }
 java {
-    sourceCompatibility = JavaVersion.toVersion("17")
-    targetCompatibility = JavaVersion.toVersion("17")
 }
 
 
@@ -62,3 +64,6 @@ micronaut {
 
 
 
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/jonathan_zollinger/Application.kt
@@ -0,0 +1,10 @@
+package jonathan_zollinger
+
+
+import io.micronaut.runtime.Micronaut.run
+
+fun main(args: Array<String>) {
+
+    run(*args)
+
+}
